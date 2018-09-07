993081ff7a27c0a83f7a4c54797911295c161f5d
Pratyush Shukla <33803125+Pratyush-S@users.noreply.github.com>
2018-09-07 21:20:41 +0530

    testing1

---
+++ brightnesscomparision_arduino_vs_delayed_function/brightnesscomparision_arduino_vs_delayed_function.ino
@@ -0,0 +1,16 @@
+void setup() {
+pinMode(2,OUTPUT);
+pinMode(3,OUTPUT);
+}
+
+void loop() {
+int T=10000;
+int t=100;
+analogWrite(3,2);
+while(1){  
+digitalWrite(2,HIGH);
+delayMicroseconds(t);
+digitalWrite(2,LOW);  
+delayMicroseconds(T-t);
+}
+}
