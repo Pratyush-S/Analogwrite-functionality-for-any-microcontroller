d64acb2e6646594e0e6db281fbcb83535cc51f04
Pratyush Shukla <33803125+Pratyush-S@users.noreply.github.com>
2018-09-08 04:24:14 +0530

    test2
    .c file changed

--- brightnesscomparision_arduino_vs_delayed_function/brightnesscomparision_arduino_vs_delayed_function.ino
+++ brightnesscomparision_arduino_vs_delayed_function/brightnesscomparision_arduino_vs_delayed_function.ino
@@ -5,7 +5,7 @@ pinMode(3,OUTPUT);
 
 void loop() {
 int T=10000;
-int t=100;
+int t=10;
 analogWrite(3,2);
 while(1){  
 digitalWrite(2,HIGH);
---
+++ timmer test.c
@@ -0,0 +1,17 @@
+#include<REGX52.H>
+
+void main()
+{
+	
+	//Timmer configuration started
+	
+	TMOD=0X12;
+	ET0=1;
+	ET1=1;
+	TF0=0;
+	TF1=0;
+	TH1=
+	TL1=
+	
+	TL0=;
+	//timmer configured
