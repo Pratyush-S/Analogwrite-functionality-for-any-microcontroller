c2d017fa3aae3d31eb2a5bc8514445b59c0819e7
Pratyush Shukla <33803125+Pratyush-S@users.noreply.github.com>
2018-09-08 05:59:45 +0530

    keil
    timmer test.c(61): error C249: 'DATA': SEGMENT TOO LARGE

--- timmer test.c
+++ timmer test.c
@@ -1,29 +1,38 @@
 #include<REGX52.H>
+//P2_0
 
-int a[303];
+int a[103];
 int led,index=1;
+
 void timmer0_10ms(void) interrupt 1
-{ led=a[index]
-	if(index>=303)index=0;
+{ led=a[index];
+	if(index>=103)index=0;
 	else index++;
-	while(TF1!=1);
-	TF0=1;
-	TF1=1;
 	
-	TF0=1;
-	TF1=1;
+	while(TF1!=1);//can be ommited
+	TF0=0;
+	TF1=0;
+	
+	TR0=1; //starting both the timmers again
+	TR1=1;
 	
 }
 
 void timmer1_10us(void) interrupt 3
 {
+	if(led<=0)P2_0=1;
+	else			P2_0=0;
+	led=led-1;
 	
+	//resetting flag and autoreloading
+	TF1=0;
+	TR1=1;
 }
 
 void main()
 {	
     int i,b=0;
-    for(i=1;i<=303;i++)
+    for(i=1;i<=103;i++)
     {
         a[i]=b;
         if((i%3)==0)b++;
@@ -31,7 +40,7 @@ void main()
 	
 	//Timmer configuration started
 	
-	TMOD=0X12;
+	TMOD=0x12;
 	//enable timmer interupt 
 	ET0=1;
 	ET1=1;
@@ -41,13 +50,12 @@ void main()
 	TF1=0;
 	
 	
-	TH1=0x000a;
-	TL1=0x0000;
-	TL0=0xd8ef;  //55535
+	TH1=0xf5;
+	//TL1=0xf5;
+	
+	TH0=0xd8;
+	TL0=0xef;  //55535
 	//timmer configured
 	TR0=1;
 	TR1=1;
-
-}
-	
-	
+}	
